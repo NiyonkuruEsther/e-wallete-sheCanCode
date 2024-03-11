c13220f84706590408f0ef82923c0d741d1bfc8f
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-03-11 14:56:35 +0200

    configure firebase

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDP4NhOY8uQ5-MnNZLDDiQHuZJr92usWc4",
-  authDomain: "muvi-app-bb33f.firebaseapp.com",
-  projectId: "muvi-app-bb33f",
-  storageBucket: "muvi-app-bb33f.appspot.com",
-  messagingSenderId: "760467440051",
-  appId: "1:760467440051:web:5169a72892fb6aaa660bc6"
+  apiKey: "AIzaSyAu22vxNa-YyAQ1QomjXZAlYtOBOerGuyk",
+  authDomain: "e-wallet-shecancode.firebaseapp.com",
+  projectId: "e-wallet-shecancode",
+  storageBucket: "e-wallet-shecancode.appspot.com",
+  messagingSenderId: "846757638778",
+  appId: "1:846757638778:web:17c322d8ec554d1b7e41c3",
+  measurementId: "G-BQDFRP173N"
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
